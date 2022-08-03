8abc8d201e83319e4ca6c6bc9845435428d5c232
yaghoubi <yaghoubi@persiatc.com>
2022-08-03 11:23:08 +0430

    edit 1

--- Modules/User/Http/Controllers/Operator/UserController.php
+++ Modules/User/Http/Controllers/Operator/UserController.php
@@ -15,7 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::where('first_name', '!=', 'super-admin')->paginate(20);
+        $users = User::where('first_name', '!=', 'super-admin')
+        ->whereNotIn('id', [1,2,3])
+        ->paginate(20);
         return view('user::operator.user.index', compact('users'));
     }
 
