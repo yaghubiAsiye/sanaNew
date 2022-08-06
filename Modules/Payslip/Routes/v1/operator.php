da446f8dc4b101a377191b50efac120ed07701da
yaghoubi <yaghoubi@persiatc.com>
2022-08-06 14:16:47 +0430

    request added edit1

--- Modules/Payslip/Routes/v1/operator.php
+++ Modules/Payslip/Routes/v1/operator.php
@@ -6,7 +6,11 @@ use Modules\Payslip\Http\Controllers\Operator\PayslipController;
 /*                                 Payslip Operator route                       */
 /* -------------------------------------------------------------------------- */
 Route::controller(PayslipController::class)->middleware('auth')->group(function(){
-    Route::get('Payslip', 'index')->name('Payslip.index');
+    // Route::get('Payslip', 'index')->name('Payslip.index');
     Route::get('Payslip/create', 'create')->name('Payslip.create');
     Route::post('Payslip/store', 'store')->name('Payslip.store');
 });
+
+Route::get('Payslip/create', function() {
+    dd(get_class(new PayslipController()));
+})->name('Payslip.index');
