16fe9653de41ffddf60d27ce4ea958f840487515
yaghoubi <yaghoubi@persiatc.com>
2022-09-18 13:52:07 +0430

    announsement edit

--- resources/views/pages/site/home.blade.php
+++ resources/views/pages/site/home.blade.php
@@ -706,7 +706,7 @@
                         </div> --}}
                         <!-- END: Recent Activities -->
                         <!-- BEGIN: Important Notes -->
-                        {{-- @if(count($announcements) > 0)
+                        @if(count($announcements) > 0)
                         <div class="col-span-12 md:col-span-12 xl:col-span-12 xl:col-start-1 xl:row-start-1 xxl:col-start-auto xxl:row-start-auto mt-3">
                             <div class="intro-x flex items-center h-10">
                                 <h2 class="text-lg font-medium truncate ml-auto">
@@ -737,7 +737,7 @@
                                     </div>
                                 </div>
                         </div>
-                        @endif --}}
+                        @endif
 
                         {{-- @if(count($dataRequests) > 0)
                         <div class="col-span-12 md:col-span-12 xl:col-span-12 xl:col-start-2 xl:row-start-2 xxl:col-start-auto xxl:row-start-auto mt-3">
